beda4705813b8212ae1dddbca7b2fc1355c4a593
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2024-10-28 21:11:22 +0300

    add test SignInWithWrongCredentials

--- src/test/java/com/netflix/tests/LoginTest.java
+++ src/test/java/com/netflix/tests/LoginTest.java
@@ -8,6 +8,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
+    private static final String LOGIN = "john@gmal.com";
+    private static final String PASSWORD = "123456";
+
     @Test
     public void testSignInWithEmptyLoginAndPassword() {
         WebDriver driver = new ChromeDriver();
@@ -24,7 +27,7 @@ public class LoginTest {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.netflix.com/login");
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.enterPassword("123456");
+        loginPage.enterPassword(PASSWORD);
         loginPage.clickSignInButton();
 
         Assertions.assertEquals(LoginMessage.EMPTY_LOGIN_FIELD, loginPage.getLoginFieldErrorMessage(), "Неверный текст ошибки валидации для пустого логина");
@@ -35,9 +38,21 @@ public class LoginTest {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.netflix.com/login");
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.enterLogin("john@gmal.com");
+        loginPage.enterLogin(LOGIN);
         loginPage.clickSignInButton();
 
         Assertions.assertEquals(LoginMessage.EMPTY_PASSWORD_FIELD, loginPage.getPasswordFieldErrorMessage(), "Неверный текст ошибки валидации для пустого пароля");
+    }
+
+    @Test
+    public void testSignInWithWrongCredentials() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.netflix.com/login");
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.enterLogin(LOGIN);
+        loginPage.enterPassword(PASSWORD);
+        loginPage.clickSignInButton();
+
+        Assertions.assertEquals(LoginMessage.WRONG_CREDENTIALS + LOGIN, loginPage.getWrongCredentialsErrorMessage(), "Неверный текст ошибки при невалидных credentials");
     }
 }
