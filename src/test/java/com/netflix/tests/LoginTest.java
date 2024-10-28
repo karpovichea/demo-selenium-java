947e9c1e2822cf50eda6303900426a0dd20e4ebc
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2024-10-28 20:50:28 +0300

    add tests SignInWithEmptyLogin and SignInWithEmptyPassword

--- src/test/java/com/netflix/tests/LoginTest.java
+++ src/test/java/com/netflix/tests/LoginTest.java
@@ -18,4 +18,26 @@ public class LoginTest {
         Assertions.assertEquals(LoginMessage.EMPTY_LOGIN_FIELD, loginPage.getLoginFieldErrorMessage(), "Неверный текст ошибки валидации для пустого логина");
         Assertions.assertEquals(LoginMessage.EMPTY_PASSWORD_FIELD, loginPage.getPasswordFieldErrorMessage(), "Неверный текст ошибки валидации для пустого пароля");
     }
+
+    @Test
+    public void testSignInWithEmptyLogin() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.netflix.com/login");
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.enterPassword("123456");
+        loginPage.clickSignInButton();
+
+        Assertions.assertEquals(LoginMessage.EMPTY_LOGIN_FIELD, loginPage.getLoginFieldErrorMessage(), "Неверный текст ошибки валидации для пустого логина");
+    }
+
+    @Test
+    public void testSignInWithEmptyPassword() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.netflix.com/login");
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.enterLogin("john@gmal.com");
+        loginPage.clickSignInButton();
+
+        Assertions.assertEquals(LoginMessage.EMPTY_PASSWORD_FIELD, loginPage.getPasswordFieldErrorMessage(), "Неверный текст ошибки валидации для пустого пароля");
+    }
 }
