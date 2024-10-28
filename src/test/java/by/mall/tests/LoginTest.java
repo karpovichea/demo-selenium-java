1070fc32ce0627fad89d52e9a6e38d635a38e50c
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2024-10-28 21:59:07 +0300

    add class LoginTest.java add test signInWithEmptyPhoneNumberAndPassword

---
+++ src/test/java/by/mall/tests/LoginTest.java
@@ -0,0 +1,25 @@
+package by.mall.tests;
+
+import by.mall.pages.LoginMessage;
+import by.mall.pages.LoginPage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+    private static final String PHONE_NUMBER = "293334455";
+    private static final String PASSWORD = "123456";
+
+    @Test
+    @DisplayName("Тест: пустой номер телефона и пароль")
+    public void testSignInWithEmptyPhoneNumberAndPassword() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://emall.by/login/password");
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.clickSignInButton();
+
+        Assertions.assertEquals(LoginMessage.EMPTY_PHONE_NUMBER_AND_PASSWORD_FIELDS, loginPage.getWrongCredentialsErrorMessage(), "Неверный текст ошибки валидации для пустого номера телефона и пароля");
+    }
+}
