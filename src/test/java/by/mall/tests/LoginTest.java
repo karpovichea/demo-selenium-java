adc052a2874ac2e7dd6aa0ef012929d542c8b708
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2024-10-28 22:01:58 +0300

    add test signInWithEmptyPhoneNumber

--- src/test/java/by/mall/tests/LoginTest.java
+++ src/test/java/by/mall/tests/LoginTest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
-    private static final String PHONE_NUMBER = "293334455";
+    private static final String PHONE_NUMBER = "+375293334455";
     private static final String PASSWORD = "123456";
 
     @Test
@@ -21,5 +21,17 @@ public class LoginTest {
         loginPage.clickSignInButton();
 
         Assertions.assertEquals(LoginMessage.EMPTY_PHONE_NUMBER_AND_PASSWORD_FIELDS, loginPage.getWrongCredentialsErrorMessage(), "Неверный текст ошибки валидации для пустого номера телефона и пароля");
+    }
+
+    @Test
+    @DisplayName("Тест: пустой номер телефона")
+    public void testSignInWithEmptyPhoneNumber() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://emall.by/login/password");
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.enterPassword(PASSWORD);
+        loginPage.clickSignInButton();
+
+        Assertions.assertEquals(LoginMessage.EMPTY_PHONE_NUMBER_FIELD, loginPage.getWrongCredentialsErrorMessage(), "Неверный текст ошибки валидации для пустого номера телефона");
     }
 }
