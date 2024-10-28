c2470032b7c397e09bead002a95cfa31c0460b78
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2024-10-28 21:11:06 +0300

    refactor LoginXpath.java и LoginPage.java

--- src/test/java/com/netflix/pages/LoginPage.java
+++ src/test/java/com/netflix/pages/LoginPage.java
@@ -3,12 +3,18 @@ package com.netflix.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LoginPage {
     private WebDriver driver;
+    private WebDriverWait wait;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     public void enterLogin(String login) {
@@ -37,7 +43,7 @@ public class LoginPage {
     }
 
     public String getWrongCredentialsErrorMessage() {
-        WebElement textMessage = driver.findElement(By.xpath(LoginXpath.WRONG_CREDENTIALS_ERROR_MESSAGE));
+        WebElement textMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(LoginXpath.WRONG_CREDENTIALS_ERROR_MESSAGE)));
         return textMessage.getText();
     }
 }
--- src/test/java/com/netflix/pages/LoginXpath.java
+++ src/test/java/com/netflix/pages/LoginXpath.java
@@ -6,5 +6,5 @@ public class LoginXpath {
     public static final String SIGN_IN_BUTTON = "/html/body/div[1]/div/div/div[2]/div/form/button[1]";
     public static final String LOGIN_FIELD_ERROR_MESSAGE = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/div[2]";
     public static final String PASSWORD_FIELD_ERROR_MESSAGE = "/html/body/div[1]/div/div/div[2]/div/form/div[2]/div/div[2]";
-    public static final String WRONG_CREDENTIALS_ERROR_MESSAGE = "/html/body/div[1]/div/div/div[2]/div/header/div";
+    public static final String WRONG_CREDENTIALS_ERROR_MESSAGE = "/html/body/div[1]/div/div/div/div/header/div/div/div/div";
 }
