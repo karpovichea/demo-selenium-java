c186ade901fdc3b56f5d4c0136aaa606fded3b8e
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2024-10-28 23:18:31 +0300

    refactor LoginTest.java

--- src/test/java/gov/login/secure/LoginTest.java
+++ src/test/java/gov/login/secure/LoginTest.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.sql.SQLOutput;
 import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
