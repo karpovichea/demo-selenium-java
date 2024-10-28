1f45b3eda7d3548e1c6882e49b92b10308b64a04
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2024-10-28 20:32:51 +0300

    refactor class LoginXpath.java

--- src/test/java/com/netflix/pages/LoginXpath.java
+++ src/test/java/com/netflix/pages/LoginXpath.java
@@ -1,10 +1,10 @@
 package com.netflix.pages;
 
 public class LoginXpath {
-    public static final String LOGIN_FIELD= "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/label";
+    public static final String LOGIN_FIELD = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/label";
     public static final String PASSWORD_FIELD = "/html/body/div[1]/div/div/div[2]/div/form/div[2]/div/div[1]/input";
     public static final String SIGN_IN_BUTTON = "/html/body/div[1]/div/div/div[2]/div/form/button[1]";
-    public static final String ERROR_LOGIN_FIELD_MESSAGE = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/div[2]";
-    public static final String ERROR_PASSWORD_FIELD_MESSAGE = "/html/body/div[1]/div/div/div[2]/div/form/div[2]/div/div[2]";
-    public static final String ERROR_CREDENTIALS_MESSAGE = "/html/body/div[1]/div/div/div[2]/div/header/div";
+    public static final String LOGIN_FIELD_ERROR_MESSAGE = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/div[2]";
+    public static final String PASSWORD_FIELD_ERROR_MESSAGE = "/html/body/div[1]/div/div/div[2]/div/form/div[2]/div/div[2]";
+    public static final String WRONG_CREDENTIALS_ERROR_MESSAGE = "/html/body/div[1]/div/div/div[2]/div/header/div";
 }
