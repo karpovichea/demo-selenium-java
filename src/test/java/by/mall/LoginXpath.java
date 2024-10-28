db69cd6d5a6a430f7e02f5ded803e7bf9c3c2dd7
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2024-10-28 21:35:48 +0300

    add class LoginXpath.java

---
+++ src/test/java/by/mall/LoginXpath.java
@@ -0,0 +1,8 @@
+package by.mall;
+
+public class LoginXpath {
+    public static final String PHONE_NUMBER_FIELD = "/html/body/div[1]/div/div/div[1]/form/div[1]/input";
+    public static final String PASSWORD_FIELD = "/html/body/div[1]/div/div/div[1]/form/div[2]/input";
+    public static final String SIGN_IN_BUTTON = "/html/body/div[1]/div/div/div[1]/form/button";
+    public static final String WRONG_CREDENTIALS_ERROR_MESSAGE = "/html/body/div[1]/div/div/div[1]/form/div[3]";
+}
