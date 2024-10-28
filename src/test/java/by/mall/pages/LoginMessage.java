1ace92f243564c3919f7beadb02ab6299d4474ca
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2024-10-28 21:58:38 +0300

    refactor code

--- src/test/java/by/mall/pages/LoginMessage.java
+++ src/test/java/by/mall/pages/LoginMessage.java
@@ -3,7 +3,7 @@ package by.mall.pages;
 public class LoginMessage {
     public static final String EMPTY_PHONE_NUMBER_FIELD = "Значение поля \"Номер телефона\" должно начинаться с +375 затем код (25|29|33|44) и далее 7 цифр (первая из которых не 0)";
     public static final String EMPTY_PASSWORD_FIELD = "Поле \"Пароль\" обязательно для заполнения.";
-    public static final String EMPTY_PHONE_NUMBER_AND_PASSWORD_FIELDS = "Значение поля \"Номер телефона\" должно начинаться с +375 затем код (25|29|33|44) и далее 7 цифр (первая из которых не 0)\n" +
+    public static final String EMPTY_PHONE_NUMBER_AND_PASSWORD_FIELDS = "Поле \"Номер телефона\" обязательно для заполнения.\n" +
             "Поле \"Пароль\" обязательно для заполнения.";
     public static final String WRONG_CREDENTIALS = "Неверный логин или пароль";
 }
