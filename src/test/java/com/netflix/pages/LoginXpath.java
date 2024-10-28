9d3881f8b2b288dd4a6921208d12dfce7d790303
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2024-10-28 20:50:07 +0300

    change xpath for login field

--- src/test/java/com/netflix/pages/LoginXpath.java
+++ src/test/java/com/netflix/pages/LoginXpath.java
@@ -1,7 +1,7 @@
 package com.netflix.pages;
 
 public class LoginXpath {
-    public static final String LOGIN_FIELD = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/label";
+    public static final String LOGIN_FIELD = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/div/input";
     public static final String PASSWORD_FIELD = "/html/body/div[1]/div/div/div[2]/div/form/div[2]/div/div[1]/input";
     public static final String SIGN_IN_BUTTON = "/html/body/div[1]/div/div/div[2]/div/form/button[1]";
     public static final String LOGIN_FIELD_ERROR_MESSAGE = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/div[2]";
