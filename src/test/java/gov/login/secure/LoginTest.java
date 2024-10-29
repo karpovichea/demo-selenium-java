cb4594cba01a8c3b6269d4977d6b2530837442d1
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2024-10-29 21:16:51 +0300

    refactor LoginTest.java

--- src/test/java/gov/login/secure/LoginTest.java
+++ src/test/java/gov/login/secure/LoginTest.java
@@ -17,12 +17,12 @@ public class LoginTest {
         WebDriver driver = new ChromeDriver();
         driver.get("https://secure.login.gov/");
 
-        WebElement buttonSignInWebElement = driver.findElement(By.xpath("html/body/main/div/form/lg-captcha-submit-button/lg-spinner-button/button"));
+        WebElement buttonSignInWebElement = driver.findElement(By.name("button"));
         buttonSignInWebElement.click();
 
         String expectedEmptyFieldErrorMessage = "This field is required";
-        String actualEmptyEmailErrorMessage = driver.findElement(By.xpath("/html/body/main/div/form/lg-validated-field/div/div")).getText();
-        String actualEmptyPasswordErrorMessage = driver.findElement(By.xpath("/html/body/main/div/form/lg-password-toggle/lg-validated-field/div/div")).getText();
+        String actualEmptyEmailErrorMessage = driver.findElement(By.xpath("//div[@class='margin-bottom-4 email required user_email validated-field__input-wrapper']/div")).getText();
+        String actualEmptyPasswordErrorMessage = driver.findElement(By.xpath("//lg-password-toggle//div[@class='usa-error-message']")).getText();
 
         Assertions.assertEquals(expectedEmptyFieldErrorMessage, actualEmptyEmailErrorMessage);
         Assertions.assertEquals(expectedEmptyFieldErrorMessage, actualEmptyPasswordErrorMessage);
@@ -34,14 +34,14 @@ public class LoginTest {
         WebDriver driver = new ChromeDriver();
         driver.get("https://secure.login.gov/");
 
-        WebElement inputEmailAddressWebElement = driver.findElement(By.xpath("/html/body/main/div/form/lg-validated-field/div/input"));
+        WebElement inputEmailAddressWebElement = driver.findElement(By.id("user_email"));
         inputEmailAddressWebElement.sendKeys("testA@test.com");
 
-        WebElement buttonSignInWebElement = driver.findElement(By.xpath("html/body/main/div/form/lg-captcha-submit-button/lg-spinner-button/button"));
+        WebElement buttonSignInWebElement = driver.findElement(By.name("button"));
         buttonSignInWebElement.click();
 
         String expectedEmptyFieldErrorMessage = "This field is required";
-        String actualEmptyPasswordErrorMessage = driver.findElement(By.xpath("/html/body/main/div/form/lg-password-toggle/lg-validated-field/div/div")).getText();
+        String actualEmptyPasswordErrorMessage = driver.findElement(By.className("usa-error-message")).getText();
 
         Assertions.assertEquals(expectedEmptyFieldErrorMessage, actualEmptyPasswordErrorMessage);
     }
@@ -52,14 +52,14 @@ public class LoginTest {
         WebDriver driver = new ChromeDriver();
         driver.get("https://secure.login.gov/");
 
-        WebElement inputPasswordWebElement = driver.findElement(By.xpath("/html/body/main/div/form/lg-password-toggle/lg-validated-field/div/input"));
+        WebElement inputPasswordWebElement = driver.findElement(By.xpath("//input[@name='user[password]']"));
         inputPasswordWebElement.sendKeys("1q2w#E4r");
 
-        WebElement buttonSignInWebElement = driver.findElement(By.xpath("html/body/main/div/form/lg-captcha-submit-button/lg-spinner-button/button"));
+        WebElement buttonSignInWebElement = driver.findElement(By.name("button"));
         buttonSignInWebElement.click();
 
         String expectedEmptyFieldErrorMessage = "This field is required";
-        String actualEmptyEmailErrorMessage = driver.findElement(By.xpath("/html/body/main/div/form/lg-validated-field/div/div")).getText();
+        String actualEmptyEmailErrorMessage = driver.findElement(By.className("usa-error-message")).getText();
 
         Assertions.assertEquals(expectedEmptyFieldErrorMessage, actualEmptyEmailErrorMessage);
     }
@@ -71,13 +71,13 @@ public class LoginTest {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://secure.login.gov/");
 
-        WebElement inputEmailAddressWebElement = driver.findElement(By.xpath("html/body/main/div/form/lg-validated-field/div/input"));
+        WebElement inputEmailAddressWebElement = driver.findElement(By.id("user_email"));
         inputEmailAddressWebElement.sendKeys("testB@test.com");
 
-        WebElement inputPasswordWebElement = driver.findElement(By.xpath("/html/body/main/div/form/lg-password-toggle/lg-validated-field/div/input"));
+        WebElement inputPasswordWebElement = driver.findElement(By.name("user[password]"));
         inputPasswordWebElement.sendKeys("a1S@d3F$");
 
-        WebElement buttonSignInWebElement = driver.findElement(By.xpath("html/body/main/div/form/lg-captcha-submit-button/lg-spinner-button/button"));
+        WebElement buttonSignInWebElement = driver.findElement(By.name("button"));
         buttonSignInWebElement.click();
 
         String expectedWrongCredentialsErrorMessage = "The email or password you’ve entered is wrong. Try resetting your password.";
