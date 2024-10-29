bc0cae4d8061877657a922d1918950942538baa0
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2024-10-29 21:59:46 +0300

    refactor LoginPage

--- src/test/java/com/netflix/pages/LoginPage.java
+++ src/test/java/com/netflix/pages/LoginPage.java
@@ -3,13 +3,9 @@ package com.netflix.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class LoginPage {
-    private WebDriver driver;
+    private final WebDriver driver;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
